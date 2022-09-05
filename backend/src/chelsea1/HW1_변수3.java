ae7e9b9ba169d42d3d27343c930eec405ef814a4
dlxow <dlxow@218.155.170.199>
2022-09-06 00:43:48 +0900

    java hw day 1

---
+++ backend/src/chelsea1/HW1_변수3.java
@@ -0,0 +1,33 @@
+package chelsea1;	// 패키지명
+
+public class HW1_변수3 {	// class s
+
+	public static void main(String[] args) {	// main s
+		// 1
+		int value;
+		value = 10;
+		System.out.println(value);
+		
+		// 2
+		int hour = 3;
+		int minute = 5;
+		System.out.println( hour + "시간" + minute +"분");
+		
+		// 3
+		int totalMinte = ( hour*60) + minute;
+		System.out.println("총" +totalMinte + "분");
+		
+		// 4
+		int x = 3;
+		int y = 5;
+		System.out.println("x : " + x + " , y :" + y);
+		
+		// 5
+		int temp = x;
+		x = y ;
+		y = temp;
+		System.out.println("x : " + x + " , y :" + y);
+		
+	}	// main e
+		
+}	// class e
---
+++ backend/src/chelsea1/HW2_자료형3.java
@@ -0,0 +1,49 @@
+package chelsea1;
+
+import java.nio.file.spi.FileSystemProvider;
+
+public class HW2_자료형3 {	// class s
+
+	public static void main(String[] args) {	// main s
+		
+		// 1.
+		byte 바이트변수1 = -128; System.out.println("바이트변수2 : " + 바이트변수1 );
+		byte 바이트변수2 = -127; System.out.println("바이트변수2 : " + 바이트변수2 );
+		// 2.
+		char 문자변수1 = 65; 			System.out.println("문자변수1 : " + 문자변수1);
+		char 문자변수2 = 'A'; 		System.out.println("문자변수2 : " + 문자변수2);
+		int	 문자변수3 = 'A'; 		System.out.println("문자변수3 : " + 문자변수3);
+		char 문자변수4 = '가'; 		System.out.println("문자변수4 : " + 문자변수4);
+		int	 문자변수5 = '가'; 		System.out.println("문자변수5 : " + 문자변수5);
+		// 3.
+		short 쇼트변수1 = 30000;		System.out.println("쇼트변수1 : " + 쇼트변수1);
+		// 4.
+		int 인트변수1 = 1000000000; 	System.out.println("인트변수1 : " + 인트변수1);
+		// 5.
+		long 롱변수1 = 1000000000; 	System.out.println("롱변수1 : " + 롱변수1);
+		long 롱변수2 = 3000000000L; 	System.out.println("롱변수2 : " + 롱변수2);
+		// 6.
+		float 실수1 = 3.14F; 		System.out.println("실수1 : " + 실수1);
+		double 실수2 = 3.14; 		System.out.println("실수2 : " + 실수2);
+		double 실수3 = 0.11545454; 	System.out.println("실수3 : " + 실수3);
+		// ?????
+		
+		// 7.
+		boolean 논리변수1 = true; System.out.println("논리변수1" + 논리변수1);
+		
+		// 8.
+		// int var1 = ??????? 
+		/*
+		 	????????????
+		 	2진수[ 1011 --int(10진수)-->11] :11
+			8진수[ 206 --int(10진수)-->134] :134
+			10진수[ 365 --int(10진수)-->365] :365
+			16진수[ b3 --int(10진수)-->179] :179
+		  
+		 */
+		
+		
+		
+	}	// main e
+	
+}	// class e
