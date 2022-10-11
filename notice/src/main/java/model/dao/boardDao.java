3810a2f11cf552d8525192ca64e1e88e30c1a948
504 <504@DESKTOP-CRHLONI>
2022-10-11 17:59:01 +0900

    Dao,Dto

--- notice/src/main/java/model/dao/boardDao.java
+++ notice/src/main/java/model/dao/boardDao.java
@@ -1,5 +1,33 @@
 package model.dao;
 
+import model.dto.boardDto;
+
 public class boardDao {
+	
+	private static boardDao bdao = new boardDao();
+	public static boardDao getInstance() { return bdao;}
+
+	private Object ps;
+	
+	// 1.
+	public boolean board( boardDto dto ) {
+		String sql = "insert into"
+				+ "board(bid ,btitle,"
+				+"bcontents,"
+				+ "bwriter,"
+				+ "bpassword," 
+				+" values(?,?,?,?,?)";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString( 1 , dto.getBid() );
+			ps.setString( 2 , dto.getBtitle() );
+			ps.setString( 3 , dto.getBcontents() );
+			ps.setString( 4 , dto.getBwriter() );
+			ps.setString( 5 , dto.getBpassword() );
+		}catch (Exception e) {System.out.println(e);}
+		return false;
+				
+	}
+	
 
 }
--- notice/src/main/java/model/dto/boardDto.java
+++ notice/src/main/java/model/dto/boardDto.java
@@ -1,5 +1,96 @@
 package model.dto;
 
+import java.util.Objects;
+
 public class boardDto {
+	// 
+	private int bno;
+	private String bid ;
+	private String btitle ;
+	private String bcontents ;
+	private String bwriter ;
+	private String bpassword ;
+	
+	//
+	public boardDto(int bno, String bid, String btitle, String bcontents, String bwriter, String bpassword) {
+		super();
+		this.bno = bno;
+		this.bid = bid;
+		this.btitle = btitle;
+		this.bcontents = bcontents;
+		this.bwriter = bwriter;
+		this.bpassword = bpassword;
+	}
+	//
+
+	public int getBno() {
+		return bno;
+	}
+
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+
+	public String getBid() {
+		return bid;
+	}
+
+	public void setBid(String bid) {
+		this.bid = bid;
+	}
+
+	public String getBtitle() {
+		return btitle;
+	}
+
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+
+	public String getBcontents() {
+		return bcontents;
+	}
+
+	public void setBcontents(String bcontents) {
+		this.bcontents = bcontents;
+	}
+
+	public String getBwriter() {
+		return bwriter;
+	}
+
+	public void setBwriter(String bwriter) {
+		this.bwriter = bwriter;
+	}
+
+	public String getBpassword() {
+		return bpassword;
+	}
+
+	public void setBpassword(String bpassword) {
+		this.bpassword = bpassword;
+	}
+	//
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(bcontents, bid, bno, bpassword, btitle, bwriter);
+	}
+
+	public boardDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
