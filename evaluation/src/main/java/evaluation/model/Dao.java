2b63b6d0d58a4d5e2ce6c83e6d09756c94cad524
504 <504@DESKTOP-CRHLONI>
2022-10-19 17:58:53 +0900

    평가

---
+++ evaluation/src/main/java/evaluation/model/Dao.java
@@ -0,0 +1,24 @@
+package evaluation.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Dao {
+
+	Connection con;
+	PreparedStatement ps;
+	ResultSet rs;
+	
+	public Dao() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); 
+			con = DriverManager.getConnection(
+						"jdbc:mysql://localhost:3306/evaluation",
+						"root",
+						"1234");
+		}catch (Exception e) { System.out.println(e);}
+	}
+	
+}
